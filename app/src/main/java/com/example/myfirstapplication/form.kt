cc2c5fd6b094b4242e6ed5886ae855e93d475480
emoRussiunGirl <yelizaveta.cherepanova.04@mail.ru>
2023-03-28 23:26:04 +0300

    пук пук

--- app/src/main/java/com/example/myfirstapplication/form.kt
+++ app/src/main/java/com/example/myfirstapplication/form.kt
@@ -2,10 +2,24 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class form : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+
+        var spinner = findViewById<Spinner>(R.id.spinner);
+
+
+        var adapter =
+        ArrayAdapter.createFromResource(this, R.array.day,
+            android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
     }
+
 }
